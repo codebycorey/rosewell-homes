bee5cf0c92ec33afa268d590713a9d184ed00496
Corey O'Donnell <me@coreyodonnell.com>
2020-08-07 21:31:13 -0400

    Add Fathom tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
-import { AppProps } from "next/app";
+import { useEffect } from 'react'
+import { AppProps } from 'next/app';
+import { useRouter, NextRouter } from "next/router";
+import * as Fathom from 'fathom-client';
 
-import "../styles/global.css";
+import '../styles/global.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const router: NextRouter = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('JROPLQNX', {
+      includedDomains: ['www.rosewellhomes.com'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  });
   return <Component {...pageProps} />;
 }
